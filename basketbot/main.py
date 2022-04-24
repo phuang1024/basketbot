725c4d97242211c935c5c9b697dd8da777985607
Patrick Huang <huangpatrick16777216@gmail.com>
2022-04-24 15:07:03 -0700

    Detect ping.

--- basketbot/main.py
+++ basketbot/main.py
@@ -3,6 +3,23 @@ import discord
 client = discord.Client()
 
 
+@client.event
+async def on_ready():
+    print("BasketBot started.")
+
+
+@client.event
+async def on_message(msg):
+    pinged = False
+    for m in msg.mentions:
+        if m.bot == True and m.name == "BasketBot":
+            pinged = True
+            break
+
+    if pinged:
+        await msg.channel.send("You pinged me.")
+
+
 if __name__ == "__main__":
     with open("token.txt", "r") as fp:
         token = fp.read().strip()
